b267917b88947cb7d9af0a5a3f267e0116eb0a53
Timothy Chen <timothy.chen188@gmail.com>
2026-03-08 21:06:45 +0000

    refactor(main): improve comments for socket creation and options

--- main.cpp
+++ main.cpp
@@ -6,14 +6,19 @@
 
 int main()
 {
+    // Create a socket
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0)
+
+    if (fd < 0) // Check if socket creation was successful
     {
-        perror("socket");
-        exit(1);
+        perror("socket"); // Print error message
+        exit(1);          // Exit with error code
     }
 
+    // Set socket options to allow reuse of the address
     int val = 1;
+    // This allows the socket to be quickly reused after it is closed, which is useful for server applications that need to restart frequently.
     setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+
     return 0;
 }
